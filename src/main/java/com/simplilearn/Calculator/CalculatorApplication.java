dde95ff16a461811e3b0ba0086d2376257629268
student <trinidad.chacon@ngc.com>
2021-06-05 21:01:58 +0000

    Introducing an error

--- src/main/java/com/simplilearn/Calculator/CalculatorApplication.java
+++ src/main/java/com/simplilearn/Calculator/CalculatorApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
+		xSpringApplication.run(CalculatorApplication.class, args);
 	}
 
 }
