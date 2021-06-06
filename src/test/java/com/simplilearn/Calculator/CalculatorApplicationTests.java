d0c6149ceeed61625a89af565ae6181854955884
student <trinidad.chacon@ngc.com>
2021-06-06 00:29:25 +0000

    Updated Tests

--- src/test/java/com/simplilearn/Calculator/CalculatorApplicationTests.java
+++ src/test/java/com/simplilearn/Calculator/CalculatorApplicationTests.java
@@ -7,7 +7,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 class CalculatorApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads1() {
+	}
+
+	@Test
+	void contextLoads2() {
+	}
+
+	@Test
+	void contextLoads3() {
 	}
 
 }
