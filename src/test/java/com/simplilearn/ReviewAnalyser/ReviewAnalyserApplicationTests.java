953343b79ba25e7a7ad9a5dc13842fdafc707761
student <trinidad.chacon@ngc.com>
2021-06-06 02:40:50 +0000

    Adding Tests

---
+++ src/test/java/com/simplilearn/ReviewAnalyser/ReviewAnalyserApplicationTests.java
@@ -0,0 +1,21 @@
+package com.simplilearn.ReviewAnalyser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewAnalyserApplicationTests {
+
+    private ReviewAnalyserApplication analyser = new ReviewAnalyserApplication();
+
+    @Test	 
+    public void testWordCount() {     	 
+   	 assertEquals(7,analyser.getWordCount("Train to win in the digital economy"));	 
+    }
+
+    @Test	 
+    public void testWordCount() {     	 
+   	 assertEquals(6,analyser.getWordCount("Train to win in the digital economy"));	 
+    }
+}
