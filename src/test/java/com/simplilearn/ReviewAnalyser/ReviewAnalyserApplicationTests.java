ede047f05ba10baec88dbf14c7f1d189d4bf2822
student <trinidad.chacon@ngc.com>
2021-06-06 02:47:05 +0000

    Update

--- src/test/java/com/simplilearn/ReviewAnalyser/ReviewAnalyserApplicationTests.java
+++ src/test/java/com/simplilearn/ReviewAnalyser/ReviewAnalyserApplicationTests.java
@@ -1,6 +1,6 @@
 package com.simplilearn.ReviewAnalyser;
 
-import org.junit.Test;
+//import org.junit.Test;
 import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,12 +10,12 @@ class ReviewAnalyserApplicationTests {
     private ReviewAnalyserApplication analyser = new ReviewAnalyserApplication();
 
     @Test	 
-    public void testWordCount() {     	 
+    public void testWordCount1() {     	 
    	 assertEquals(7,analyser.getWordCount("Train to win in the digital economy"));	 
     }
 
     @Test	 
-    public void testWordCount() {     	 
+    public void testWordCount2() {     	 
    	 assertEquals(6,analyser.getWordCount("Train to win in the digital economy"));	 
     }
 }
