10f6faebeba12e3f66f9102157f154a0c7080c91
student <trinidad.chacon@ngc.com>
2021-06-07 02:24:14 +0000

    Baseline version

--- src/main/java/com/simplilearn/ReviewAnalyser/ReviewAnalyserApplication.java
+++ src/main/java/com/simplilearn/ReviewAnalyser/ReviewAnalyserApplication.java
@@ -1,4 +1,4 @@
-package com.simplilearn.ReviewAnalyser;
+#package com.simplilearn.ReviewAnalyser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
