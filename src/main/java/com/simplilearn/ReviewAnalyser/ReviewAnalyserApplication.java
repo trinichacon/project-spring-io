ef06e45670e28e2021042d5ec80272d175266820
student <trinidad.chacon@ngc.com>
2021-06-08 05:19:35 +0000

    Update

--- src/main/java/com/simplilearn/ReviewAnalyser/ReviewAnalyserApplication.java
+++ src/main/java/com/simplilearn/ReviewAnalyser/ReviewAnalyserApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReviewAnalyserApplication {
+	//Change
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewAnalyserApplication.class, args);
